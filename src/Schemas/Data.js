006a4b13fb5bfe38c5b601cedd47fb88d07a9a47
Michiel Verbeke <michiel.verbeke777@gmail.com>
2024-05-07 17:34:18 +0200

    update

---
+++ src/Schemas/Data.js
@@ -0,0 +1,122 @@
+export const Data = {
+  profile: {
+    name: "Michiel Verbeke",
+    ocupation: "ICT Consultant - Developer",
+    location: "Beselare, België",
+    email: "michiel.verbeke777@gmail.com",
+    telephone: "0470658548",
+    image: "images/ik.jpg",
+  },
+  aboutMe: {
+    label: "Over Mij",
+    description:
+      "Ambitieuse tech (nerd) die nieuwschierig is naar het vinden van diverse applicatie oplossingen."
+  },
+  skills: {
+    technicalLabel: "Hard Skills",
+    softLabel: "Soft Skills",
+    technicalSkills: [
+      "React: JavaScript framework",
+      "Laravel: PHP framework",
+      "Python: FastAPI, Flask",
+      "Java",
+      "Solidity",
+      "Thirdweb: front-end web3 framework",
+      "SQL",
+      "Git",
+      "ERP: SAP ~ junior kennis",
+      "CRM: Salesforce ~ junior kennis",
+      "Microsoft Power Platform: Power Apps, Power Automate, Power Bi"
+    ],
+    softSkills: [
+      "Leergierig",
+      "Optimistisch",
+      "Sociaal",
+      "Diverse nieuwschierigheden zowel binnen en buiten IT"
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Stuur me een connectie op Linkedin",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/michielverbeke7",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Volg me op github",
+        name: "github",
+        url: "https://github.com/michielv7",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "STAGE ICT HELPDESK",
+        period: "Okt. 2019 - Dec. 2019",
+        company: "Jan Yperman Ziekenhuis",
+        description: [
+          "Het helpen van IT gerelateerde zaken binnenin het ziekenhuis en het innoveren van verouderde systemen.",
+          "Stage in verband mijn opleiding Informaticabeheer uit het middelbaar."
+        ],
+      },
+      {
+        title: "FULL-STACK DEVELOPER JOBSTUDENT",
+        period: "2020 - 2023",
+        company: "Optimize-IT",
+        description: [
+          "Het programmeren van full-stack applicaties met eindklant als prioriteit.",
+        ],
+      },
+      {
+        title: "STAGE",
+        period: "Feb. 2023 - Mei 2023",
+        company: "Optimize-IT",
+        description: [
+          "API ontwikkeld voor de interactie tussen een laadpaal en een systeem genaamd Loxone",
+          "Research gedaan rondom de blockchain technologie voor mijn eindwerk"
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Informaticabeheer",
+        date: "2017 - 2019",
+        institution: "Heilige Familie Ieper",
+      },
+      {
+        career: "ICT & Blockchain Consultancy (Traject Toegepaste Informatica)",
+        date: "2019 - 2023",
+        institution: "Howest Brugge"
+      },
+      {
+        career: "Industriële Wetenschappen Informatica",
+        date: "2023 - 2024",
+        institution: "Universiteit Gent"
+      }
+    ],
+    proyects: [
+      {
+        name: "RC-Car",
+        company: "Howest",
+        period: "2de semester 2022",
+        description: [
+          "Speelgoed auto laten besturen via een website met een geïntegreerde camera, deze auto was op afstand te bedienen via het internet",
+          "Kennis uit dit project: Raspberry PI, front-end: PWA, back-end: Flask (Python)"
+        ],
+      },
+      {
+        name: "Horse Athlete",
+        company: "Howest",
+        period: "1ste semester 2023",
+        description: [
+          "Groepswerk, persoonlijke ERP gebouwd voor een zadelmaker vanuit scratch als project met een externe firma.",
+          "Kennis uit dit project: front-end: NextJS, back-end: STRAPI"
+        ],
+      },
+    ],
+  },
+};
