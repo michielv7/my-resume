ec31bc80cb9f5e740dd5b4e5c0431fae8a4fca1c
Michiel Verbeke <michiel.verbeke777@gmail.com>
2024-05-21 18:02:20 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,12 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { Resume } from "./Pages/Resume";
-import global_en from "./Schemas/translations/en/global.json"
-import global_nl from "./Schemas/translations/nl/global.json"
-import i18next from "i18next";
 
 import "./App.css";
 
-i18next.init({
-    interpolation: {escapeValue: false},
-    lng: "nl",
-    resources: {
-        nl: {
-            global: global_nl,
-        },
-        en: {
-            global: global_en
-        }
-    }
-})
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <I18nextProvider>
-            <Resume />
-        </I18nextProvider>
+        <Resume />
     </React.StrictMode>,
 );
 
